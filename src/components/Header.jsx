6b94aeaa89bf9965cd9540b2cbe814682cfcc48d
Hunter-og <floflower436@gmail.com>
2024-09-02 08:40:55 -0700

    UPDATED

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Header() { 
-    const menubar = [ 'HOME' , 'HOW IT WORKS', 'EXPLORE', 'Reviews', 'Blog', 'contact' ]
+    const menubar = [ 'HOME' , 'HOW IT WORK', 'EXPLORE', 'Reviews', 'BLOG', 'contact' ]
   return (
     <div className=' flex items-center justify-between  h-[10vh] pr-10 pl-10 sticky shadow bg-white top-0  z-1000 '>
         <div>
