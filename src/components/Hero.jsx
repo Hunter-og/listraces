cff8788365c88e8d22c3eb1d6ebffe1caf336b37
Hunter-og <floflower436@gmail.com>
2024-09-03 08:49:38 -0700

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ export default function Hero() {
       <h1 className=' text-white text-[40px] font-bold text-center '>BEST PLACE TO FIND AND EXPLORE <br /> THAT ALL YOU NEED</h1>
         <p className=' text-white mt-10 text-lg font-semibold' >Find Best Place,Restaurant,Hotel,Real State and many more think in just One click</p>
         </div>
-      <div className=" flex items-center">
+      {/* <div className=" flex items-center">
         <p className=' bg-white py-5'>what??</p>
       <input type="" name="" id="" placeholder='Ex: Palce,Resturent,Food,Automobile' className="bar bg-white h-[10vh] w-[100%] rounded-[5px] " />
       
-      </div>
+      </div> */}
       
     
      </div>
